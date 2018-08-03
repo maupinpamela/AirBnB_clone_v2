93057b810588b9f745722d64bbcd024400bfa387
maupinpamela <maupinpamela@gmail.com>
2018-08-03 23:47:49 +0000

    added slash

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -23,6 +23,6 @@ def do_deploy(archive_path):
     run("mv {}/web_static/* {}/".format(new_path, new_path))
     run("rm -rf {}/web_static".format(new_path))
     run("rm -rf /data/web_static/current")
-    run("ln -s {} /data/web_static/current".format(new_path))
+    run("ln -s {}/ /data/web_static/current".format(new_path))
     print ("New version deployed!")
     return True
