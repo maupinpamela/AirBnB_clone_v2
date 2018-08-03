6e537c2bbe90581d98f924d51f5df4f0b07db468
maupinpamela <maupinpamela@gmail.com>
2018-08-03 20:29:29 +0000

    added semicolon and slash

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,12 +3,11 @@
 
 sudo apt-get update -y
 sudo apt-get install nginx -y
-sudo service nginx start
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo "Holberton" | sudo tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-locations="\\\nlocation /hbnb_static {alias /data/web_static/current/}"
+locations="\\\nlocation /hbnb_static/ {alias /data/web_static/current/;}"
 sed -i "37i $locations" /etc/nginx/sites-available/default
 sudo service nginx restart
