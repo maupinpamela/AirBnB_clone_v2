7d9f613658246b9bd7bff2adb9fb7ffb59d54f13
maupinpamela <maupinpamela@gmail.com>
2018-08-03 23:48:48 +0000

    pycode checks

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""Fabric script (based on the file 1-pack_web_static.py) that
-distributes an archive to your web servers, using the function do_deploy """
-
+"""Fabric script (based on the file 1-pack_web_static.py)"""
 from fabric.api import run, put, env
 import os
 
