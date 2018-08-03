968f4b7196ae0d8f877340726c42763a14e49e64
maupinpamela <maupinpamela@gmail.com>
2018-08-03 17:08:46 +0000

    sets up your web servers for the deployment of web_static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# sets up your web servers for the deployment of web_static
+
+sudo apt-get update -y
+sudo apt-get install nginx -y
+sudo service nginx start
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton" | sudo tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+locations="\\\nlocation /hbnb_static {alias /data/web_static/current/}"
+sed -i "37i $locations" /etc/nginx/sites-available/default
+sudo service nginx restart
