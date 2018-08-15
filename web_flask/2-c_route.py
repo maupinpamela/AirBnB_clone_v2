315925239ec0cd0efcbc522deb80b79423ee42b8
maupinpamela <maupinpamela@gmail.com>
2018-08-15 21:56:41 +0000

    /c/<text>: display “C ” followed by the value of the text variable (replace underscore _ symbols with a space

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""hbnb: display “HBNB” """
+
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def holberton():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def fun(text):
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
